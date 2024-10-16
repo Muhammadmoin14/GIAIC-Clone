3af0c8fec309e53f75b64f5558acf74a66251a0d
Muhammad Moin <162424469+Muhammadmoin14@users.noreply.github.com>
2024-10-16 15:49:48 +0500

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Link from 'next/link'
+
+
+
 
 function Header() {
   return (
@@ -6,11 +10,11 @@ function Header() {
         <img src="/logo.png" alt="logo" className="h-24 top-5  px-24 "/>
         <h1 className="font-bold  text-2xl "style={{color:"#99bbd1"}}>Tuition Free Education Program on Latest Technologies</h1>
         <ul className="flex space-x-8 justify-end px-24 ">
-        <li>Home</li>
-        <li>Apply</li>
-        <li>Jobs</li>
-        <li>Result</li>
-        <li>Course</li>
+        <Link href="/"><li>Home</li></Link>
+        <Link href="/apply"><li>Apply</li></Link>
+        <Link href="/jobs"><li>Jobs</li></Link>
+        <Link href="/result"><li>Result</li></Link>
+        <Link href="/course"><li>Course</li></Link>
         </ul>
   </div>
   )
